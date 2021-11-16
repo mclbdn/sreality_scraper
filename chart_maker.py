9a336feeb9dea90ec3ccddb983ede1c0916437c8
mclbdn <michalbednar93@gmail.com>
2021-11-16 09:20:36 -0600

    Change chart title

--- chart_maker.py
+++ chart_maker.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-pd.options.display.float_format = "{:,.2f}".format
-
 class ChartMaker:
     def __init__(self, csv_name: str, num_of_pages: int) -> None:
         self.csv_name = csv_name
         self.num_of_pages = num_of_pages
 
     def make_a_bar_chart(self):
+        pd.options.display.float_format = "{:,.2f}".format
         df = pd.read_csv(f"{self.csv_name}.csv")
         df["price_in_kc"] = df["price_in_kc"].astype(float)
         grouped_df = df.groupby("locality")
@@ -16,7 +15,7 @@ class ChartMaker:
         mean_df = mean_df.reset_index()
         plt.ticklabel_format(style="plain")
         plt.bar(mean_df["locality"], mean_df["price_in_kc"])
-        plt.title(f"Prices of flats in Prague based on Sreality.cz offers\n(first {self.num_of_pages} pages)", fontsize=14)
+        plt.title(f"Average price of a flat in Prague based on Sreality.cz offers\n(first {self.num_of_pages} pages)", fontsize=14)
         plt.xlabel("Locality", fontsize=14)
         plt.ylabel("Price", fontsize=14)
         current_values = plt.gca().get_yticks()
